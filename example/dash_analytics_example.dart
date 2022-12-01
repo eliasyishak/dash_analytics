de9fbfabeb39cbbbf72707908c1e3bd7a7b42b82
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2022-12-01 12:27:08 -0500

    refactoring away from singleton design to globally initialized

--- example/dash_analytics_example.dart
+++ example/dash_analytics_example.dart
@@ -2,23 +2,8 @@ import 'dart:io';
 
 import 'package:dash_analytics/dash_analytics.dart';
 
-void main() async {
-  DateTime start = DateTime.now();
-  print('###### START ###### $start');
-  String? home;
-  Map<String, String> envVars = Platform.environment;
-
-  if (Platform.isMacOS) {
-    home = envVars['HOME'];
-  } else if (Platform.isLinux) {
-    home = envVars['HOME'];
-  } else if (Platform.isWindows) {
-    home = envVars['UserProfile'];
-  }
-
-  final Directory homeDirectory = Directory(home!);
-  final int flutterToolsMessageVersion = 1;
-  final String flutterToolsMessage = '''
+final int flutterToolsMessageVersion = 1;
+final String flutterToolsMessage = '''
 The [tool name] uses Google Analytics to report usage and diagnostic data
 along with package dependencies, and crash reporting to send basic crash
 reports. This data is used to help improve the Dart platform, Flutter framework,
@@ -32,26 +17,46 @@ If you opt out of telemetry, an opt-out event will be sent, and then no further
 information will be sent. This data is collected in accordance with the
 Google Privacy Policy (https://policies.google.com/privacy).
 ''';
-  final String tool = 'flutter-tools';
+final String tool = 'flutter-tools';
 
-  final String measurementId = 'G-N1NXG28J5B';
-  final String apiSecret = '4yT8__oER3Cd84dtx6r-_A';
-  // TODO: look into filtering out requests coming into GA4 around conditionals
-  //  found within the body of the request
+// TODO: look into filtering out requests coming into GA4 around conditionals
+//  found within the body of the request
+final String measurementId = 'G-N1NXG28J5B';
+final String apiSecret = '4yT8__oER3Cd84dtx6r-_A';
 
-  // Initialize the singleton + run the initializer to produce
-  // the files for telemetry if they don't exist
-  Analytics.setup(
-    tool: tool,
-    homeDirectory: homeDirectory,
-    measurementId: measurementId,
-    apiSecret: apiSecret,
-    toolsMessageVersion: flutterToolsMessageVersion,
-    toolsMessage: flutterToolsMessage,
-    branch: 'ey-test-branch',
-    flutterVersion: 'Flutter 3.6.0-7.0.pre.47',
-    dartVersion: 'Dart 2.19.0',
-  );
+Directory getHomeDirectory() {
+  String? home;
+  Map<String, String> envVars = Platform.environment;
+
+  if (Platform.isMacOS) {
+    home = envVars['HOME'];
+  } else if (Platform.isLinux) {
+    home = envVars['HOME'];
+  } else if (Platform.isWindows) {
+    home = envVars['UserProfile'];
+  }
+
+  return Directory(home!);
+}
+
+// Globally instantiate the analytics class at the entry
+// point of the tool
+final Analytics analytics = Analytics(
+  tool: tool,
+  homeDirectory: getHomeDirectory(),
+  measurementId: measurementId,
+  apiSecret:
+      apiSecret, // TODO: determine if this can live within the package or remain passed in
+  toolsMessageVersion: flutterToolsMessageVersion,
+  toolsMessage: flutterToolsMessage,
+  branch: 'ey-test-branch',
+  flutterVersion: 'Flutter 3.6.0-7.0.pre.47',
+  dartVersion: 'Dart 2.19.0',
+);
+
+void main() async {
+  DateTime start = DateTime.now();
+  print('###### START ###### $start');
 
   DateTime end = DateTime.now();
   print(
--- lib/src/analytics.dart
+++ lib/src/analytics.dart
@@ -1,23 +1,7 @@
 import 'initializer.dart';
 
 class Analytics {
-  static final Analytics _instance = Analytics._internal();
-
-  bool _singletonInitialized = false;
-
-  factory Analytics() {
-    if (!_instance._singletonInitialized) {
-      throw Exception(
-          'Analytics has not been initialized - to initialize call `Analytics.setup(...)` '
-          'before calling for the singleton instance\n'
-          "Recommended to call at entry point's main method");
-    }
-    return _instance;
-  }
-
-  /// This will be required to setup before using the
-  /// [Analytics] singleton instance.
-  static void setup({
+  Analytics({
     required tool,
     required homeDirectory,
     required measurementId,
@@ -40,13 +24,5 @@ class Analytics {
       forceReset: forceReset ?? false,
     );
     initializer.run();
-
-    // This will allow for the singleton to be access via the
-    // factory constructor
-    _instance._singletonInitialized = true;
   }
-
-  /// Produces the singleton instance of the analytics
-  /// class to be passed around tools codebase
-  Analytics._internal();
 }
