389c51eee5cb11ac7696dc2110ca222dba9bb726
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-01-09 17:46:50 -0500

    clean up

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,26 +1,3 @@
-/// The version number for the message below
-///
-/// If the message below is altered, the version should
-/// be incremented so that users can be prompted with
-/// the updated messaging
-const int kToolsMessageVersion = 1;
-
-/// The message that should be shown to the user
-const String kToolsMessage = '''
-Flutter and Dart related tooling uses Google Analytics to report usage and
-diagnostic data along with package dependencies, and crash reporting to
-send basic crash reports. This data is used to help improve the Dart
-platform, Flutter framework, and related tools.
-
-Telemetry is not sent on the very first run.
-To disable reporting of telemetry, run this terminal command:
-
-[dart|flutter] --disable-telemetry.
-If you opt out of telemetry, an opt-out event will be sent, and then no further
-information will be sent. This data is collected in accordance with the
-Google Privacy Policy (https://policies.google.com/privacy).
-''';
-
 /// The string that will provide the boilerplate for the
 /// configuration file stored on the user's machine
 const String kConfigString = '''
@@ -59,3 +36,26 @@ reporting=1
 # and the value is a date in the form YYYY-MM-DD, a comma, and
 # a number representing the version of the message that was
 # displayed.''';
+
+/// The message that should be shown to the user
+const String kToolsMessage = '''
+Flutter and Dart related tooling uses Google Analytics to report usage and
+diagnostic data along with package dependencies, and crash reporting to
+send basic crash reports. This data is used to help improve the Dart
+platform, Flutter framework, and related tools.
+
+Telemetry is not sent on the very first run.
+To disable reporting of telemetry, run this terminal command:
+
+[dart|flutter] --disable-telemetry.
+If you opt out of telemetry, an opt-out event will be sent, and then no further
+information will be sent. This data is collected in accordance with the
+Google Privacy Policy (https://policies.google.com/privacy).
+''';
+
+/// The version number for the message below
+///
+/// If the message below is altered, the version should
+/// be incremented so that users can be prompted with
+/// the updated messaging
+const int kToolsMessageVersion = 1;
